9df26a5216b5c80b1cbaa881c89952041d197e08
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2023-02-21 17:32:15 +0600

    Update exetension imageView+URl.swift

--- hw4Month4/Models/exetension imageView+URl.swift
+++ hw4Month4/Models/exetension imageView+URl.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension UIImageView {
+    func getImage(from path: String) {
+        guard let url = URL(string: path) else { return }
+        Task {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            DispatchQueue.main.async {
+            self.image = UIImage(data: data)
+            }
+        }
+    }
+}
