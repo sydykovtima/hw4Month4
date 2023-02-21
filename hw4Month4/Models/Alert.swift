52a855d1ab39eb1653a34afb94d3e7f3af140b9a
tima <timka.0416@gmail.com>
2023-02-21 15:14:16 +0600

    finish project

---
+++ hw4Month4/Models/Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  hw4Month4
+//
+//  Created by Mac on 21/2/2023.
+//
+
+import Foundation
---
+++ hw4Month4/Models/exetension imageView+URl.swift
@@ -0,0 +1,8 @@
+//
+//  exetension imageView+URl.swift
+//  hw4Month4
+//
+//  Created by Mac on 20/2/2023.
+//
+
+import Foundation
