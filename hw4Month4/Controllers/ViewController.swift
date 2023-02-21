35efc7dc7358162b1540fdcee93f9cd6e67f5010
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2023-02-21 17:24:38 +0600

    Update ViewController.swift

--- hw4Month4/Controllers/ViewController.swift
+++ hw4Month4/Controllers/ViewController.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
     
     @IBOutlet private weak var searchProduct: UISearchBar!
@@ -16,7 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet private weak var categoryCollectionView: UICollectionView!
     @IBOutlet private weak var productTableView: UITableView!
     
-//    private var productArray: [Product] = []
     private var categoryArray: [Category] = []
     private var orderType: [TypeOfOrder] = []
     private var products: [Product] = []
@@ -30,10 +27,9 @@ class ViewController: UIViewController {
         categoryCollectionView.showsHorizontalScrollIndicator = false
         productTableView.showsVerticalScrollIndicator = false
         productTableView.showsHorizontalScrollIndicator = false
-//        fetchProducts()
-//        fetchcategory()
-//        fetchOrderType()
-        fetchProduct()
+        fetchProducts()
+        fetchOrderType()
+        fetchCategory()
     }
     
     private func configureCategoryCV() {
@@ -43,12 +39,17 @@ class ViewController: UIViewController {
         categoryCollectionView.delegate = self
         typeOfOrderCollectionView.dataSource = self
         typeOfOrderCollectionView.delegate = self
-        typeOfOrderCollectionView.register(UINib(nibName:
-        String(describing: OrderTypeCollectionViewCell.self),
-        bundle: nil), forCellWithReuseIdentifier:
-        OrderTypeCollectionViewCell.reuseIdentifierForOrderType)
+        typeOfOrderCollectionView.register (
+        UINib(
+        nibName: String(describing: OrderTypeCollectionViewCell.self),
+        bundle: nil
+        ),
+        forCellWithReuseIdentifier:
+        OrderTypeCollectionViewCell.reuseIdentifierForOrderType
+        )
         categoryCollectionView.register(UINib(nibName: String(
-       describing: CategoryCollectionViewCell.self), bundle: nil),
+        describing:
+        CategoryCollectionViewCell.self), bundle: nil),
         forCellWithReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier)
         productTableView.register(UINib(nibName:
         String( describing: ProductTableViewCell.self)
@@ -56,34 +57,26 @@ class ViewController: UIViewController {
         ProductTableViewCell.reuseIdentifier)
     }
     
-//    private func fetchProducts() {
-//        do { productArray = try NetworkLayer.shared.fetchProducts()
-//            productTableView.reloadData()
-//        }catch {
-//            print("error \(error.localizedDescription)")
-//        }
-//    }
-//    
-//    private func fetchcategory() {
-//        do { categoryArray = try NetworkLayer.shared.fetchCategory()
-//           categoryCollectionView.reloadData()
-//        }catch {
-//            print("error \(error.localizedDescription)")
-//        }
-//    }
-//    
-//    private func fetchOrderType() {
-//        do { orderType = try NetworkLayer.shared.fetchOrderType()
-//            typeOfOrderCollectionView.reloadData()
-//        } catch {
-//            print("error \(error.localizedDescription)")
-//        }
-//    }
+    private func fetchCategory() {
+        do { categoryArray = try NetworkLayer.shared.fetchCategory()
+            categoryCollectionView.reloadData()
+        }catch {
+            print("error \(error.localizedDescription)")
+        }
+    }
     
-    private func fetchProduct() {
+    private func fetchOrderType() {
+        do { orderType = try NetworkLayer.shared.fetchOrderType()
+            typeOfOrderCollectionView.reloadData()
+        } catch {
+            print("error \(error.localizedDescription)")
+        }
+    }
+    
+    private func fetchProducts() {
         isLoading = true
         
-        NetworkLayer.shared.fetchProducts { result in
+        NetworkLayer.shared.fetchProducts{ result in
             self.isLoading = false
             switch result {
             case .success(let model):
@@ -92,21 +85,23 @@ class ViewController: UIViewController {
                     self.productTableView.reloadData()
                 }
             case .failure(let error):
-                self.showError(with: error)
+                DispatchQueue.main.async {
+                    self.showError(with: error)
+                }
             }
         }
     }
     
-    private func searchProduct(by word: String) {
+    private func searchProducts(by word: String) {
         isLoading = true
         
         NetworkLayer.shared.searchProducts(by: word) { result in
             self.isLoading = false
             switch result {
             case .success(let model):
-                self.products = model
-                DispatchQueue.main.async {
-                    self.productTableView.reloadData()
+            self.products = model
+            DispatchQueue.main.async {
+            self.productTableView.reloadData()
                 }
             case .failure(let error):
                 self.showError(with: error)
@@ -114,98 +109,187 @@ class ViewController: UIViewController {
         }
     }
     
+    
     private func showError(with message: Error) {
-        let alert = UIAlertController(title: "Error", message: message.localizedDescription, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Error",
+         message: message.localizedDescription,
+         preferredStyle: .alert)
         alert.addAction(.init(title: "Okay", style: .cancel))
         present(alert, animated: true)
     }
 }
 
-    extension ViewController: UITableViewDataSource {
-        func tableView(_ tableView: UITableView,
-        numberOfRowsInSection section: Int
-        ) -> Int {
-            products.count
-        }
-        
-        func tableView(_ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-        ) -> UITableViewCell {
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+     numberOfRowsInSection section: Int
+    ) -> Int {
+        products.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
         let cell = productTableView.dequeueReusableCell(
-        withIdentifier: ProductTableViewCell.reuseIdentifier,
-        for: indexPath
-            ) as! ProductTableViewCell
-            let model = products[indexPath.row]
+         withIdentifier: ProductTableViewCell.reuseIdentifier,
+         for: indexPath
+        ) as! ProductTableViewCell
+        let model = products[indexPath.row]
+        cell.display(item: model)
+        cell.delegate = self
+        print("cell created")
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView,
+     heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        return 350
+    }
+    func tableView(_ tableView: UITableView, 
+     canEditRowAt indexPath: IndexPath
+     ) -> Bool {
+        return true
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            handleDeleteTakeaways(indexPath: indexPath)
+        }
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "Create new product") 
+            { _, _, _ in
+            self.askUserConfirmAlert(for: indexPath, deleteAction: false)
+        }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    private func handleDeleteTakeaways(indexPath:IndexPath) {
+        askUserConfirmAlert(for: indexPath, deleteAction: true)
+    }
+    
+    private func deleteProducts(by id: Int) {
+        NetworkLayer.shared.deleteProductsData(id: id) { result in
+            switch result {
+            case .success(_) :
+                DispatchQueue.main.async {
+                    self.showSuccessAlert()
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showError(with: error)
+                }
+            }
+        }
+    }
+    
+    private func askUserConfirmAlert(for indexPath: IndexPath, deleteAction: Bool) {
+        if deleteAction {
+            let alert = UIAlertController(title: "Вы действительн хотите удалить?",
+            message: "Данное действие нельзя вернуть обратно", preferredStyle: .alert)
+            let yesAction = UIAlertAction(title: "Да", style: .destructive) { [unowned self] action in
+            
+                let id = self.products[indexPath.row].id
+                self.deleteProducts(by: id)
+                self.products.remove(at: indexPath.row)
+                self.productTableView.reloadData()
+                
+            }
+            alert.addAction(yesAction)
+            alert.addAction(UIAlertAction(title: "нет", style: .default))
+            present(alert, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Хотите создать новый продукт?", message: nil, preferredStyle: .alert)
+            let yesAction = UIAlertAction(title: "Да", style: .default) { [unowned self] action in
+                let vc = storyboard?.instantiateViewController(withIdentifier: "second_vc")
+                as! SecondViewController
+                vc.product = products[indexPath.row]
+                present(vc, animated: true)
+            }
+            alert.addAction(yesAction)
+            alert.addAction(UIAlertAction(title: "нет", style: .default))
+            present(alert, animated: true)
+        }
+       
+    }
+    
+    private func showSuccessAlert() {
+        let alert = UIAlertController(title: "Успешно удалено",
+        message: "Продукт удален", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ОК", style: .default))
+        present(alert, animated: true)
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int
+    ) -> Int {
+        if collectionView == categoryCollectionView {
+            return categoryArray.count
+        }
+        if collectionView == typeOfOrderCollectionView {
+            
+        }
+        return orderType.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        if collectionView == categoryCollectionView {
+            let cell = collectionView.dequeueReusableCell(
+             withReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier,
+             for: indexPath
+            ) as! CategoryCollectionViewCell
+            let model = categoryArray[indexPath.row]
             cell.display(item: model)
-            cell.delegate = self
-            print("cell created")
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier:
+                    OrderTypeCollectionViewCell
+                    .reuseIdentifierForOrderType,
+                for: indexPath) as! OrderTypeCollectionViewCell
+            let model = orderType[indexPath.row]
+            cell.display(item: model)
             return cell
         }
     }
+}
 
-    extension ViewController: UITableViewDelegate {
-        func tableView(_ tableView: UITableView,
-        heightForRowAt indexPath: IndexPath
-        ) -> CGFloat {
-            return 350
-        }
-    }
-
-    extension ViewController: UICollectionViewDataSource {
-        func collectionView(_ collectionView: UICollectionView,
-             numberOfItemsInSection section: Int
-        ) -> Int {
-            if collectionView == categoryCollectionView {
-                return categoryArray.count
-            }
-            if collectionView == typeOfOrderCollectionView {
-              
-            }
-            return orderType.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-        ) -> UICollectionViewCell {
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+     layout collectionViewLayout: UICollectionViewLayout,
+     sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
         if collectionView == categoryCollectionView {
-        let cell = collectionView.dequeueReusableCell(
-        withReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier,
-        for: indexPath
-        ) as! CategoryCollectionViewCell
-        let model = categoryArray[indexPath.row]
-        cell.display(item: model)
-        return cell
-        } else {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
-        OrderTypeCollectionViewCell
-        .reuseIdentifierForOrderType,
-        for: indexPath) as! OrderTypeCollectionViewCell
-        let model = orderType[indexPath.row]
-        cell.display(item: model)
-        return cell
-            }
+            return CGSize(width: 80, height: 100)
+        } else if collectionView == typeOfOrderCollectionView {
+            
         }
+        return CGSize(width: 105, height: 80)
     }
-
-    extension ViewController: UICollectionViewDelegateFlowLayout {
-        func collectionView(_ collectionView: UICollectionView,
-             layout collectionViewLayout: UICollectionViewLayout,
-             sizeForItemAt indexPath: IndexPath
-            ) -> CGSize {
-            if collectionView == categoryCollectionView {
-                return CGSize(width: 80, height: 100)
-            } else if collectionView == typeOfOrderCollectionView {
-                
-            }
-            return CGSize(width: 105, height: 80)
-        }
-    }
+    
+}
 
 extension ViewController: ProductsCellDelegate {
     func didSelectionsProducts(item: Product) {
         let secondVC = storyboard?
-        .instantiateViewController(withIdentifier: "second_vc"
-        ) as! SecondViewController
+            .instantiateViewController(withIdentifier: "second_vc"
+            ) as! SecondViewController
         navigationController?.pushViewController(secondVC, animated: true)
     }
 }
@@ -213,7 +297,9 @@ extension ViewController: ProductsCellDelegate {
 extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if !isLoading {
-            searchProduct(by: searchText)
+            searchProducts(by: searchText)
         }
     }
 }
+
+
