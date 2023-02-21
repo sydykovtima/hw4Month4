a14489fa78d7ce7abf99cc68802e58acfc328c1a
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2023-02-21 17:30:39 +0600

    Update Products.swift

--- hw4Month4/Models/Products.swift
+++ hw4Month4/Models/Products.swift
@@ -7,56 +7,20 @@
 
 import UIKit
 
-
-//let productJSON = """
-//[{
-//        "productsImageView": "Burger Craze",
-//        "nameProducts": "Burger Craze",
-//        "openClose": "Open ",
-//        "delivery": "Free",
-//        "madeOnTheWord": "American: Burgers",
-//        "time": "1.5 km",
-//        "cost": "minimum 10$",
-//        "rate": "4.6(601)",
-//        "distance": "15-20 min"
-//},
-//{
-//        "productsImageView": "Vegetarian Pizza",
-//        "nameProducts": "Vegetarian Pizza",
-//        "openClose": "Open",
-//        "delivery": "Free",
-//        "madeOnTheWord": "American: Pizza",
-//        "time": "1.5 km",
-//        "cost": "minimum 10$",
-//        "rate": "4.6(601)",
-//        "distance": "15-20 min"
-//}
-//]
-//"""
-
-//id": 1,
-//            "title": "iPhone 9",
-//            "description": "An apple mobile which is nothing like apple",
-//            "price": 549,
-//            "discountPercentage": 12.96,
-//            "rating": 4.69,
-//            "stock": 94,
-//            "brand": "Apple",
-//            "category": "smartphones",
-//            "thumbnail": "https:/
-struct Product: Decodable {
-    let productsImageView: String
+struct Product: Codable {
+    let id: Int
+    let thumbnail: String
     let title: String
     let description: String
     let price: Int
-    let discountPercentage: String
-    let rating: String
-    let stock: String
+    let discountPercentage: Double
+    let rating: Double
+    let stock: Int
     let brand: String
     let category: String
-    
 }
 
-struct Products: Decodable {
-    let product: [Product]
+struct Products: Codable {
+    let products: [Product]
 }
+
